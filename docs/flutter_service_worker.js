cef7e2957cd66ddf461b2216d6a5e090fb15056e
Marcus Vinícius <marcusviniciusbp@gmail.com>
2021-05-10 08:17:58 -0300

    nova versão do build web

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,15 +4,15 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "0c57cc2977b9eac6c7fbe9d081358f8b",
-"index.html": "84aab74b94dc6ae6f1ba4107bd1dd11f",
-"/": "84aab74b94dc6ae6f1ba4107bd1dd11f",
-"main.dart.js": "448da35a4138311911059bc41eb770b1",
+"index.html": "07f4a13e6850c6e39f94e4202b339659",
+"/": "07f4a13e6850c6e39f94e4202b339659",
+"main.dart.js": "818e54807445fc28f00a64c53c3509b0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "57fe43f471370f638efde0ed93bdebc2",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "c2c35209f684eaa8136e02885f94fc64",
+"assets/NOTICES": "0bd4854b24b471914a11ba6a11a9761e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac"
