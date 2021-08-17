681753548da3b4a081f71d05799b0d48e465a5c4
Marcus Vinícius <marcusviniciusbp@gmail.com>
2021-08-17 09:13:23 -0300

    apagado arquivos do projeto

--- test/app/contas/widgets/add_conta/add_form_widget_test.dart
+++
@@ -1,7 +0,0 @@
-// import 'package:penny_saver/app/contas/widgets/add_conta/add_form_widget.dart';
-import 'package:flutter_test/flutter_test.dart';
-// import 'package:flutter_modular_test/flutter_modular_test.dart';
-
-main() {
-  group('AddFormWidget', () {});
-}
